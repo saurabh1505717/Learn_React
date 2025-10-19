94e3da0adc843797f7de0c1a2fa09e484d5b9c64
saurabh <sauravparmar878@gmail.com>
2025-10-19 09:35:58 +0530

    created custom react, and learned about how actually react creates html element, and how to pass evaluated expression(variable) while creating a react element

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,14 +1,16 @@
 import Chai from './chai.jsx'
 
 function App() {
+  const username = "Saurabh the dev"
 
   return (
     <>
       <Chai />
-      <p>hi Para this side</p>
+      <p>hi Para this side with {username}</p>
       <h1>I am a H1 tag, present inside a fragment. AS JSX allows exporting only one element. Therefore, we are wrappwed inside a FRAGMENT TAG</h1>
     </>
   )
 }
+// {username} - this is called as evaluated expression
 
 export default App
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import App from "./App";
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function MyApp (){
+  return (
+    <div>
+      <h1>Hey Broooo!!!</h1>
+    </div>
+  )
+}
+
+// This will not work as we have not written any custom render code for this, and React has its own render function where the expected type of object is not like this, in our custom_React, we had a custom render code
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+// instead we can do something like 
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+// if we want to inject variable in the below reactElement, then we can do it like, 
+const anotherUser = 'The developer: Saurabh'
+
+// let's create it using REACT 
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click to visit google',
+  anotherUser // evaluated expression
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <MyApp/>
+
+    // anotherElement
+
+    // reactElement
+
     <App />
-  </StrictMode>,
 )
